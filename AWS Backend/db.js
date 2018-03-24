bae6b8e2170f542ac43b01fb18aeff22355fdab6
Daniel Thompson <dlt0012@auburn.edu>
2018-03-24 04:03:12 -0500

    Updated to 3.0.0-rc0 MongoDB driver.

--- AWS Backend/db.js
+++ AWS Backend/db.js
@@ -1,3 +1,4 @@
+"use strict";
 // Using the database design described in the MEAN stack:
 // https://www.mongodb.com/blog/post/the-modern-application-stack-part-2-using-mongodb-with-nodejs
 
@@ -5,21 +6,23 @@ const MongoClient = require('mongodb').MongoClient;
 
 class DB {
     constructor() {
+        this.client = null;
         this.db = null;
     }
-    async connect(uri) {
+    async connect(uri, dbName) {
         if (this.db) { return; }
         try {
-            this.db = await MongoClient.connect(uri);
-        } catch(err) {
+            this.client = await MongoClient.connect(uri);
+            this.db = this.client.db(dbName);
+        } catch (err) {
             console.log("Error connecting: " + err.message);
             throw(err);
         }
     }
     async close() {
-        if (this.db) {
+        if (this.client) {
             try {
-                this.db.close();
+                 return this.client.close();
             } catch(err) {
                 console.log("Failed to close the database: " + err.message);
                 throw(err);
@@ -29,14 +32,14 @@ class DB {
     async addDocument(coll, document) {
         let collection;
         try {
-            collection = await this.db.collection(coll, {strict: false});
-        } catch(err) {
+            collection = this.db.collection(coll, {strict: false});
+        } catch (err) {
             console.log("Could not access collection: " + err.message);
             throw(err);
         }
         try {
-            collection.insertOne(document, {w: "majority"});
-        } catch(err) {
+            return await collection.insertOne(document, {w: "majority"});
+        } catch (err) {
             console.log("Insert failed: " + err.message);
             throw(err);
         }
