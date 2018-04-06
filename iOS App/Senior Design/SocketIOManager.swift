a84995574f18ec415b0d1e3f70f779533d9d297c
Daniel Thompson <dlt0012@auburn.edu>
2018-04-06 11:56:58 -0500

    Refactor database URLs into constants

--- iOS App/Senior Design/SocketIOManager.swift
+++ iOS App/Senior Design/SocketIOManager.swift
@@ -5,5 +5,31 @@
 //  Created by Daniel Thompson on 4/5/18.
 //  Copyright © 2018 Senior Design. All rights reserved.
 //
+//  Modified from example code on:
+//  https://www.twilio.com/blog/2016/09/getting-started-with-socket-io-in-swift-on-ios.html
+//
 
 import Foundation
+import SocketIO
+
+class SocketIOManager {
+    static let sharedInstance = SocketIOManager()
+    var socket = SocketIOClient(socketURL: BASEURL)
+    
+    override init() {
+        super.init()
+        
+        socket.on("sensor update") { data, ack in
+            NotificationCenter.default
+                .post(name: Notification.Name(rawValue: "callSensorUpdate"), object: data[0] as? [String: AnyObject])
+        }
+    }
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+}
