1e4b2f0a45f6cd33a3cfe649c98374bd4c93176e
Daniel Thompson <dlt0012@auburn.edu>
2018-04-06 21:07:11 -0500

    Add sensor data POST to database functionality

--- AWS Backend/app.js
+++ AWS Backend/app.js
@@ -5,8 +5,6 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
 const index = require('./routes/index');
 const api = require('./routes/api');
 
@@ -18,8 +16,8 @@ app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
