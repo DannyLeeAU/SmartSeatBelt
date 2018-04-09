4031b4f12cdf5ddc83f70b95123b9276923cfd81
Daniel Thompson <dlt0012@auburn.edu>
2018-04-09 13:22:47 -0500

    Clarify interface between backend and app

--- AWS Backend/controllers/sensors.js
+++ AWS Backend/controllers/sensors.js
@@ -32,19 +32,23 @@ exports.getAllSensors = async (req, res, next) => {
 exports.postOneSensor = async (req, res, next) => {
     let database = new DB;
     try {
+        let key = req.body.key;
+        let value = req.body.value;
+        let timestamp = req.body.timestamp;
+
         let updateSeat = {};
-        updateSeat[req.body.key] = req.body.value;
-        updateSeat['timestamp'] = req.body.timestamp;
+        updateSeat[key] = value;
+        updateSeat['timestamp'] = timestamp;
 
         let updateSensor = {};
-        updateSensor[req.body.key] = {value: req.body.value, timestamp: req.body.timestamp};
+        updateSensor[key] = {value, timestamp};
 
         await database.connect(url);
         await Promise.all([
             database.updateDocumentById('Seats', req.body.id, {$set: updateSeat}),
             database.updateDocumentById('Sensors', req.body.id, {$push: updateSensor})
         ]);
-        req.io.locals.emit('sensor update', req.body.key, req.body.value, req.body.timestamp);
+        req.io.locals.emit('sensor update', {key, value, timestamp});
         res.send(true);
     } catch (err) {
         res.send(err);
