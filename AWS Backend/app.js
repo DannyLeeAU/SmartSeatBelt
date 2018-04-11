881e88fbac36ad8479dffa19ea8f9267249b4a98
Daniel Thompson <dlt0012@auburn.edu>
2018-04-11 12:25:06 -0500

    Remove unused dependencies in backend
    There were two unused dependencies in the node.js backend:
        * cookie-parser
        * body-parser
    
    'body-parser' is not needed for Express v4.16.0, and 'cookie-parser'
    is not needed since req.cookies does not need to be called.
    
    Minor cleanup was also done on views/index.js to make it consistent with
    the rest of the project.

--- AWS Backend/app.js
+++ AWS Backend/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const index = require('./routes/index');
 const api = require('./routes/api');
 
@@ -18,7 +17,6 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
