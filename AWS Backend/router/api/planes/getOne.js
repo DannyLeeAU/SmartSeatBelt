455a71a04ffd6479960b304baf858115ff26f288
Daniel Thompson <dlt0012@auburn.edu>
2018-04-26 13:17:13 -0500

    Fixed parameter name

--- AWS Backend/router/api/planes/getOne.js
+++ AWS Backend/router/api/planes/getOne.js
@@ -7,7 +7,7 @@ async function getOnePlane(req, res, next) {
     let database = DatabaseFactory.getDatabase();
     try {
         await database.connect();
-        let result = await database.getDocumentById('Planes', req.params.plane);
+        let result = await database.getDocumentById('Planes', req.params.id);
         res.send(result);
     } catch (err) {
         res.send(err);
